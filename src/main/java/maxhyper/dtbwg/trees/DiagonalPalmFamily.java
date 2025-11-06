5799b88eadf153101384dc45f40d33d0a1a5eae3
Max Hyper <massimocantugarcia@gmail.com>
2025-11-06 20:48:39 -0300

    fixed palms not growing properly

--- src/main/java/maxhyper/dtbwg/trees/DiagonalPalmFamily.java
+++ src/main/java/maxhyper/dtbwg/trees/DiagonalPalmFamily.java
@@ -3,6 +3,7 @@ package maxhyper.dtbwg.trees;
 import com.dtteam.dynamictrees.api.registry.TypedRegistry;
 import com.dtteam.dynamictrees.block.branch.BasicBranchBlock;
 import com.dtteam.dynamictrees.block.branch.BranchBlock;
+import com.dtteam.dynamictrees.block.branch.ThickBranchBlock;
 import com.dtteam.dynamictrees.block.leaves.DynamicLeavesBlock;
 import com.dtteam.dynamictrees.systems.GrowSignal;
 import com.dtteam.dynamictrees.tree.family.Family;
@@ -10,6 +11,7 @@ import com.dtteam.dynamictrees.tree.species.Species;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public class DiagonalPalmFamily extends Family {
 
@@ -21,41 +23,49 @@ public class DiagonalPalmFamily extends Family {
     
     @Override
     protected BranchBlock createBranchBlock(ResourceLocation name) {
-        final BasicBranchBlock branch = new BasicBranchBlock(name, this.getProperties()){
-
+        final BasicBranchBlock branch = isThick() ? new ThickBranchBlock(name, this.getProperties()){
             @Override
-            public GrowSignal growIntoAir(Level world, BlockPos pos, GrowSignal signal, int fromRadius) {
-                final Species species = signal.getSpecies();
-
-                final DynamicLeavesBlock leaves = species.getLeavesBlock().orElse(null);
-                if (leaves != null) {
-                    if (fromRadius == getFamily().getPrimaryThickness()) {// If we came from a twig (and we're not a stripped branch) then just make some leaves
-                        if (isNextToBranch(world, pos, signal.dir.getOpposite())){
-                            signal.success = false;
-                            return signal;
-                        }
-                        // TODO: check if this got clobbered
-                        signal.success = (leaves.growLeavesIfLocationIsSuitable(world, species.getLeavesProperties(), pos.above(), null) != 0);
-                        if (signal.success)
-                            return leaves.branchOut(world, pos, signal);
-                    } else {// Otherwise make a proper branch
-                        return leaves.branchOut(world, pos, signal);
-                    }
-                } else {
-                    //If the leaves block is null, the branch grows directly without checking for leaves requirements
-                    if (isNextToBranch(world, pos, signal.dir.getOpposite())){
-                        signal.success = false;
-                        return signal;
-                    }
-                    setRadius(world, pos, getFamily().getPrimaryThickness(), null);
-                    signal.radius = getFamily().getSecondaryThickness();
-                    signal.success = true;
-                }
-                return signal;
+            public @NotNull GrowSignal growIntoAir(Level world, BlockPos pos, GrowSignal signal, int fromRadius) {
+                return DiagonalPalmFamily.growIntoAir(world, pos, signal, fromRadius, this);
+            }
+        } : new BasicBranchBlock(name, this.getProperties()){
+            @Override
+            public @NotNull GrowSignal growIntoAir(Level world, BlockPos pos, GrowSignal signal, int fromRadius) {
+                return DiagonalPalmFamily.growIntoAir(world, pos, signal, fromRadius, this);
             }
         };
         if (this.isFireProof())
             branch.setFireSpreadSpeed(0).setFlammability(0);
         return branch;
+    }
+
+    private static GrowSignal growIntoAir(Level world, BlockPos pos, GrowSignal signal, int fromRadius, BranchBlock branchBlock) {
+        final Species species = signal.getSpecies();
+
+        final DynamicLeavesBlock leaves = species.getLeavesBlock().orElse(null);
+        if (leaves != null) {
+            if (fromRadius == branchBlock.getFamily().getPrimaryThickness()) {// If we came from a twig (and we're not a stripped branch) then just make some leaves
+                if (BranchBlock.isNextToBranch(world, pos, signal.dir.getOpposite())){
+                    signal.success = false;
+                    return signal;
+                }
+                // TODO: check if this got clobbered
+                signal.success = (leaves.growLeavesIfLocationIsSuitable(world, species.getLeavesProperties(), pos.above(), 0) != 0);
+                if (signal.success)
+                    return leaves.branchOut(world, pos, signal);
+            } else {// Otherwise make a proper branch
+                return leaves.branchOut(world, pos, signal);
+            }
+        } else {
+            //If the leaves block is null, the branch grows directly without checking for leaves requirements
+            if (BranchBlock.isNextToBranch(world, pos, signal.dir.getOpposite())){
+                signal.success = false;
+                return signal;
+            }
+            branchBlock.setRadius(world, pos, branchBlock.getFamily().getPrimaryThickness(), null);
+            signal.radius = branchBlock.getFamily().getSecondaryThickness();
+            signal.success = true;
+        }
+        return signal;
     }
 }
